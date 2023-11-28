2ab7dbde229b0579f53a4831e32e15447fea1809
hunaisong <hunaisong@vipkid.com.cn>
2023-11-28 11:42:05 +0800

    feat: 动态规划-打家劫舍，二维数组的方式求解

--- 198.打家劫舍.js
+++ 198.打家劫舍.js
@@ -24,19 +24,61 @@ var rob = function(nums) {
    * dp[j] = max(dp[j-2]+nums[j], dp[j-1])
    */
 
+  // const len = nums.length;
+  // const dp = new Array(len).fill(0);
+
+  // // 初始化
+  // dp[0] = nums[0]; // 当下标为0只能取nums[0]
+  // dp[1] = Math.max(nums[0], nums[1]); // 当下标为1，取二者0和1二者最大的值
+
+  // for (let i = 2; i < len; i++) {
+  //   // 从递推公式里来看，我们需要的i需要从2开始，即初始化0 1
+  //   dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1])
+  // }
+
+  // return dp[len-1];
+
+  /**
+   * 二维数组
+   * dp[i][0]不打劫房间i的最大金额
+   * dp[i][1]打劫房间i的最大金额
+   * 
+   * 状态转移？
+   * 对于房间i来说有两种状态
+   * 打劫
+   * 不打劫
+   * 打劫的情况: 由于相邻房间不能被打劫，所以如果是打劫的情况那么 dp[i][1] = dp[i-1][0] + nums[i];
+   * 不打劫的情况： dp[i][0] = max(dp[i-1][0], dp[i-1][1])
+   * 
+   * 一些新的想法对么？
+   * 不打劫: 那么就是说明i-1一定打劫了 dp[i][0] = dp[i-1][1];
+   * 这个说法对么，不打劫难道就说明i-1一定打劫了么，因为不确定所以只能取max(dp[i-1][0], dp[i-1][1])
+   * 
+   * 打劫: 那么说明i-1一定没打劫 dp[i][1] = dp[i-1][0] + nums[i]
+   * 这个说法是对的，因为相邻的不能同时被打劫
+   */
+
   const len = nums.length;
-  const dp = new Array(len).fill(0);
 
-  // 初始化
-  dp[0] = nums[0]; // 当下标为0只能取nums[0]
-  dp[1] = Math.max(nums[0], nums[1]); // 当下标为1，取二者0和1二者最大的值
-
-  for (let i = 2; i < len; i++) {
-    // 从递推公式里来看，我们需要的i需要从2开始，即初始化0 1
-    dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1])
+  // 构造dp数组
+  const dp = [];
+  for (let i = 0; i < len; i++) {
+    dp[i] = [];
+    dp[i][0] = 0;
+    dp[i][1] = 0;
   }
 
-  return dp[len-1];
+  // 由状态转移可以推导出i需要从1开始，那么就要初始化0的情况
+  dp[0][0] = 0
+  dp[0][1] = nums[0];
+
+  // 遍历
+  for (let i = 1; i < len; i++) {
+    dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
+    dp[i][1] = dp[i-1][0] + nums[i];
+  }
+
+  return Math.max(dp[len-1][0], dp[len-1][1]);
 };
 // @lc code=end
 

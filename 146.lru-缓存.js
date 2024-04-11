3e572c1e8178e26036a0a884f720048e1af4cf9e
hunaisong <hunaisong@vipkid.com.cn>
2024-04-11 17:12:40 +0800

    feat: 解决LRU-缓存题目，终于一次性完整无误的写出来了

--- 146.lru-缓存.js
+++ 146.lru-缓存.js
@@ -70,10 +70,40 @@
 
 // @lc code=start
 /**
+ * 说一下这个题的思路吧，这题理解起来不是很难，但是并不是很好写的一个题
+ * 我个人觉得这更算是一个hard级别的题目，可能在于比较好理解或者是现在比较卷
+ * 这个题变成了medium级别的了
+ * 
+ * 这个题目挺像是从一摞书中放书和取书
+ * 取书，就是把书从这摞书中取出一本，放在最上边
+ * 放书，分两种情况了，书如果有，就把书拿出来，放上边；如果没有，就把新书放上边
+ */
+
+
+// 创建双向链表解决此类问题
+class LinkNode {
+  constructor(prev, next, val, key) {
+    this.prev = prev;
+    this.next = next;
+    this.val = val;
+    this.key = key;
+  }
+}
+
+/**
  * @param {number} capacity
  */
 var LRUCache = function(capacity) {
-
+  this.capacity = capacity;
+  // 创建一个map，用来存key-val
+  this.nodeMap = new Map();
+  // 创建头尾节点，用来记录书堆中头尾书
+  this.head = new LinkNode();
+  this.tail = new LinkNode();
+  this.head.prev = this.tail;
+  this.head.next = this.tail;
+  this.tail.prev = this.head;
+  this.tail.next = this.head;
 };
 
 /** 
@@ -81,7 +111,11 @@ var LRUCache = function(capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-
+  if (!this.nodeMap.has(key)) return -1;
+  // 把节点移动到头部
+  const node = this.nodeMap.get(key);
+  this.moveNodeToHead(node);
+  return node.val;
 };
 
 /** 
@@ -90,8 +124,67 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-
+  // 如果节点存在
+  if (this.nodeMap.has(key)) {
+    const node = this.nodeMap.get(key);
+    node.val = value;
+    this.nodeMap.set(key, node);
+    // 移动到头部
+    this.moveNodeToHead(node);
+  } else {
+    const node = new LinkNode(null, null, value, key);
+    this.nodeMap.set(key, node);
+    // 添加到头部
+    this.addNodeToHead(node);
+    // 如果size超过capacity
+    if (this.nodeMap.size > this.capacity) {
+      // 取出尾元素
+      const node = this.tail.prev;
+      // 删除尾元素
+      this.removeTail(node);
+      // 从map中删除
+      this.nodeMap.delete(node.key);
+    }
+  }
 };
+
+// 细分一下操作
+// 1. 添加一本新书，就是把书放到头部，addNodeToHead
+// 2. 抽出一本书，等于把这本书拿出来，然后放头部，即 先 removeNode 然后 addNodeToHead
+// 3. 超出capacity了，就意味着需要找到尾部书，然后去掉，即 找尾 然后 removeNode
+// 总结一下：1. 添加新书 2. 抽出一本书放头部 3. 删除一本书
+
+LRUCache.prototype.addNodeToHead = function (node) {
+  // head node x ... tail 在其中添加新的节点到头部
+  // 处理新节点的前后连接
+  node.prev = this.head;
+  node.next = this.head.next;
+  // 处理新节点与头结点的连接
+  node.prev.next = node;
+  // 处理新节点与新节点后边的连接
+  node.next.prev = node;
+}
+
+LRUCache.prototype.removeNode = function (node) {
+  // ... x1 x2 x3 ...
+  // 假如删除的是x2
+  node.prev.next = node.next;
+  node.next.prev = node.prev;
+  node.prev = null;
+  node.next = null;
+  return node;
+}
+
+LRUCache.prototype.moveNodeToHead = function (node) {
+  // 取出这个节点，等于删除
+  // 然后把这个节点加到头部
+  const _node = this.removeNode(node);
+  this.addNodeToHead(_node);
+}
+
+LRUCache.prototype.removeTail = function (node) {
+  this.removeNode(node);
+}
 
 /**
  * Your LRUCache object will be instantiated and called as such:

478e4832ad31e8faeda4b8ab63b7dae379396f3c
hunaisong <hunaisong@vipkid.com.cn>
2024-04-15 14:46:36 +0800

    feat: 今日刷题，最近的公共祖先

--- 236.二叉树的最近公共祖先.js
+++ 236.二叉树的最近公共祖先.js
@@ -19,6 +19,25 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
+  /**
+   * 有几种情况考虑
+   * p q 在两侧
+   * q是p的子树
+   * p是q的子树
+   * 
+   * 思路，如果深度搜索发现当前节点与p或者q一致，那么就返回
+   * 如果这一侧没有节点会最终走到最后的叶子节点，返回null
+   */
+  if (!root) return null;
+  if (root === p) return p;
+  if (root === q) return q;
+  // 这是如上三个条件的结合写法
+  // if (!root || root === p || root === q) return root;
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  if (left && !right) return left;
+  if (!left && right) return right;
+  if (left && right) return root;
 };
 // @lc code=end
 

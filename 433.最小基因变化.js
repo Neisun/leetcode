25f3a3c56766e7b79a5f168a3e3bcb63afbc71b5
hunaisong <hunaisong@vipkid.com.cn>
2024-04-28 19:11:17 +0800

    feat: 今日leetcode

--- 433.最小基因变化.js
+++ 433.最小基因变化.js
@@ -75,34 +75,74 @@
  * @return {number}
  */
 var minMutation = function(startGene, endGene, bank) {
-  // 判断endGene是否在bank中，如果不在bank中，必然不是有效转化
-  if (!bank.includes(endGene)) return -1;
+  // // 判断endGene是否在bank中，如果不在bank中，必然不是有效转化
+  // if (!bank.includes(endGene)) return -1;
 
-  const _startGene = Array.from(startGene);
+  // const _startGene = Array.from(startGene);
 
+  // let count = 0;
+
+  // // 每次变化的基因必须存在于bank中
+  // const stack = [_startGene];
+  // while (stack.length) {
+  //   const cur = stack.shift();
+  //   if (cur.join('') === endGene) return count;
+  //   // 与endGene做对比
+  //   for (let i = 0; i < 8; i++) {
+  //     if (cur[i] !== endGene[i]) {
+  //       cur[i] = endGene[i];
+  //       // 判断新组成的基因是不是在bank中
+  //       const newStartGene = cur.join('');
+  //       if (bank.includes(newStartGene)) {
+  //         count++;
+  //         stack.push(cur);
+  //         break;
+  //       } else {
+  //         cur[i] = startGene[i];
+  //       }
+  //     }
+  //   }
+  // }
+  // return -1;
+
+  // 上述思路是错误的，我们重新捋一下
+  // 先使用一个hash map记录一下bank中出现的基因
+  const map = new Map();
+  const visited = new Map();
+  for (const gene of bank) {
+    map.set(gene, true);
+    visited.set(gene, false);
+  }
+  // 先判断endGene是不是在bank中，如果endGene不在，意味着无法变换
+  if (!map.has(endGene)) return -1;
+
+  // 其他的情况那就是在逐步去寻找
+  // 8个位置的基因，每次每个都可以变成ACGT这四个字母中的一个
+  const stack = [startGene];
+  const chars = ['A', 'C', 'G', 'T'];
   let count = 0;
-
-  // 每次变化的基因必须存在于bank中
-  const stack = [_startGene];
   while (stack.length) {
-    const cur = stack.shift();
-    if (cur.join('') === endGene) return count;
-    // 与endGene做对比
-    for (let i = 0; i < 8; i++) {
-      if (cur[i] !== endGene[i]) {
-        cur[i] = endGene[i];
-        // 判断新组成的基因是不是在bank中
-        const newStartGene = cur.join('');
-        if (bank.includes(newStartGene)) {
-          count++;
-          stack.push(cur);
-          break;
-        } else {
-          cur[i] = startGene[i];
+    const size = stack.length;
+    for (let i = 0; i < size; i++) {
+      let cur = stack.shift();
+      if (cur === endGene) return count;
+      for (let j = 0; j < 8; j++) {
+        for (const char of chars) {
+          if (cur[j] !== char) { // 要替换的字母跟当前字母不一致才替换，做一个剪枝处理
+            const newGeneArray = Array.from(cur);
+            newGeneArray[j] = char;
+            const newGene = newGeneArray.join('');
+            if (map.has(newGene) && !visited.get(newGene)) { // 只有出现在bank中，并且没有访问过（避免多次访问，造成死循环）
+              stack.push(newGene);
+              visited.set(newGene, true);
+            }
+          }
         }
       }
     }
+    count++;
   }
+
   return -1;
 };
 // @lc code=end

6c1795bd9555df728a985d0f3d4c8bb772348eb2
hunaisong <hunaisong@vipkid.com.cn>
2024-05-29 17:07:29 +0800

    feat: 今日份leetcode

---
+++ 34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=javascript
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (42.73%)
+ * Likes:    2704
+ * Dislikes: 0
+ * Total Accepted:    995.7K
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+ * 
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ * 
+ * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * nums 是一个非递减数组
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+  while (l <= r) {
+    let mid = Math.floor((l+r)/2);
+    if (target === nums[mid]) {
+      // 找到了这个值，然后，双指针向两头走
+      let i = mid;
+      let j = mid;
+      while (nums[i] === target) {
+        i--;
+      }
+      while (nums[j] === target) {
+        j++;
+      }
+      return [i+1, j-1];
+    } else if (target < nums[mid]) {
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+  return [-1, -1];
+};
+// @lc code=end
+
